176947bdcaffe9d93903ef339489ad65a4c7aa0d
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-03-20 03:17:55 +0900

    결과 조회 API

--- src/main/java/com/hellojpa/jpql/JpaMain.java
+++ src/main/java/com/hellojpa/jpql/JpaMain.java
@@ -27,6 +27,13 @@ public class JpaMain {
             TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
             // 이렇게 하면 반환 타입이 두 개라서 명확하지 않음 -> Query타입으로 반환 받아야 함.
             Query query2 = em.createQuery("select m.username, m.age from Member m");
+            
+            // 결과가 하나 이상일 때, 리스트 반환 결과가 없으면 빈 리스트 반환
+            List<Member> resultList = query1.getResultList();
+            // 결과가 정확히 하나, 단일 객체 반환 결과가 없거나 둘 이상이면 Exception 발생
+            Member singleResult = query1.getSingleResult();
+            
+            System.out.println(singleResult);
 
             tx.commit();
         } catch (Exception e){
