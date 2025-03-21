6699b6babb7f25dd5ca9bab44dafbc7224b40f5c
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-03-21 23:41:30 +0900

    엔티티 프로젝션

--- src/main/java/com/hellojpa/jpql/JpaMain.java
+++ src/main/java/com/hellojpa/jpql/JpaMain.java
@@ -23,12 +23,14 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                            .setParameter("username", "member1")
-                            .getSingleResult();
+            em.flush();
+            em.clear();
 
-            System.out.println("result = " + result.getUsername());
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
 
+            Member findMember = result.get(0);
+            findMember.setAge(20);
 
             tx.commit();
         } catch (Exception e){
