9a85d8789f2f0a680afb0cb2e31c9ba8fdd5dbbd
Ji, jeonguk <dkdpawjddnr@naver.com>
2025-04-15 18:46:58 +0900

    컬렉션 값 연관 경로: 묵시적 내부 조인 발생, 탐색x

--- src/main/java/com/hellojpa/jpql/JpaMain.java
+++ src/main/java/com/hellojpa/jpql/JpaMain.java
@@ -1,6 +1,8 @@
 package com.hellojpa.jpql;
 
 import jakarta.persistence.*;
+
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -14,26 +16,31 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            em.persist(team);
 
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select m.team From Member m";
+            String query = "select t.members From Team t";
 
-            List<Team> result = em.createQuery(query, Team.class)
+            List<Member> result = em.createQuery(query, Member.class)
                             .getResultList();
 
-            for(Team s : result){
-                System.out.println("s = " + s);
+            for(Member m : result){
+                System.out.println("o = " + m);
             }
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
